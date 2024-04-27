af7e963f5416275e79ecbb7868d3a8cebfede78f
yamashita <g2172117@tcu.ac.jp>
2024-04-27 12:23:12 +0900

    [add]next-sitemap

--- components/SeoHead.jsx
+++ components/SeoHead.jsx
@@ -17,8 +17,8 @@ export const SeoHead = ({
     "@context": "https://schema.org",
     "@type": "WebSite",
     "url": "https://www.amausyrup.net",
-    "name": "天羽しろっぷ unofficial",
-    "description": "AmauSyrup.netは、VTuber『天羽しろっぷ』の非公式ファンサイトです。notionAPIを使用して、天羽しろっぷの最新情報をお届けします。",
+    "name": "天羽しろっぷ | AmauSyrup",
+    "description": "AmauSyrup.netは、VTuber『天羽しろっぷ』の非公式ファンサイトです。大学生,身バレ,X(Twitter)ユーザであることが有名な天羽しろっぷの最新情報をお届けします。",
     "publisher": {
       "@type": "Person",
       "name": "AmauSyrup.net",
@@ -43,9 +43,20 @@ export const SeoHead = ({
       <meta name="description" content={description} />
       <meta property="og:description" content={description} />
       <meta property="og:type" content={ogType} />
+      <meta name="keywords" content="天羽しろっぷ, 身バレ, 大学"/>
       <link rel="icon" href={"/favicon.ico"} />
+      <link rel="apple-touch-icon" href={"/favicon.ico"} />
+      <link rel="apple-touch-icon" sizes="152x152" href={"/favicon.ico"} />
+      <link rel="apple-touch-icon" sizes="180x180" href={"/favicon.ico"} />
+      <link rel="apple-touch-icon" sizes="167x167" href={"/favicon.ico"} />
+      <link rel="apple-touch-icon" sizes="120x120" href={"/favicon.ico"} />
+      <link rel="apple-touch-icon" sizes="76x76" href={"/favicon.ico"} />
+      <link rel="apple-touch-icon" sizes="60x60" href={"/favicon.ico"} />
+      <link rel="apple-touch-icon" sizes="57x57" href={"/favicon.ico"} />
+      <link rel="apple-touch-icon" sizes="72x72" href={"/favicon.ico"} />
+      <link rel="apple-touch-icon" sizes="114x114" href={"/favicon.ico"} />
+      <link rel="apple-touch-icon" sizes="144x144" href={"/favicon.ico"} />
       
-
       <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: schemaString }}></script>
     </Head>
   );
